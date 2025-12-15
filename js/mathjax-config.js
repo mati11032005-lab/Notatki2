8f4610dd46b851b62ee45e1290255f2155871034
MateuszI <mati11032005@gmail.com>
2025-12-15 21:55:52 +0100

    Deployed ef4200c with MkDocs version: 1.6.1

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,9 +1,16 @@
 window.MathJax = {
   tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+    inlineMath: [["\\(", "\\)"]],
+    displayMath: [["\\[", "\\]"]],
+    processEscapes: true,
+    processEnvironments: true
   },
   options: {
-    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex"
   }
 };
+
+document$.subscribe(() => { 
+  MathJax.typesetPromise()
+})
